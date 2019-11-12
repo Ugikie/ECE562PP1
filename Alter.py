836b19d0e030b5a05cd25fa5e10afa633f874735
gmp97700 <57686251+gmp97700@users.noreply.github.com>
2019-11-12 14:54:41 -0800

    Add files via upload

--- Alter.py
+++ Alter.py
@@ -1,4 +1,34 @@
 # Alter Bitch
+
+import random
+import numpy as np
+import math
+
+# Initially, sequence & snr_dB in 'strings'
+def snr_noise(seq,snr_dB):
+    snr_dB = float(snr_dB)
+    snr = 10**(snr_dB/10)
+    var = 1/snr
+    sigma = math.sqrt(var)
+
+    # err_seq will be 'list' of 'strings' (decimal values)
+    err_seq = np.random.normal(0, sigma, len(seq))
+
+    alt_seq = ''
+    for k in range(len(seq)):
+
+        # seq_temp will hold the kth decision value of seq
+        seq_temp = float(seq[k]) + float(err_seq[k])
+        if seq_temp <= 0.5:
+            seq_temp = '0'  # decision 1
+        else:
+            seq_temp = '1'  # decision 0
+        alt_seq = alt_seq + seq_temp
+
+    return alt_seq
+
+
+
 def seq_condition(sequence):
     seq_typ = True
     
@@ -23,7 +53,9 @@ def alter(sequence):
                     errseq = input('Alter: Type Error Sequence of only "0"s and "1"s: ')
             sequence = errseq
         elif intyp == 'r':
-            print('Alter: random')
+            snr = input('Alter: What is the desired SNR (dB) level?')
+            sequence = snr_noise(sequence,snr)
+            print(sequence)
     
         return sequence
 
